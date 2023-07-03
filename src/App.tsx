bffd5def41609fd51a6ead02804e029f0e90314f
aegir <agir.jonsson@matsmart.se>
2023-07-03 14:12:01 +0000

    tailwinding App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import Foxlympics from "./Components/Foxlympics";
 import results from "./Data/Data";
-import "./App.css";
+import classNames from "classnames";
+
+const beforeClassNames =
+  "before:content-['Vetur'] before:right-[calc(50%+60px)] before:bottom-4 before:absolute before:text-[22px] before:font-gantari";
+const afterClassNames =
+  "after:content-['Sumar'] after:left-[calc(50%+60px)] after:top-4 after:absolute after:text-[22px] after:font-gantari";
 
 const App = () => {
   return (
-    <main className="App">
+    <main className="h-full w-screen snap-x snap-mandatory flex flex-row font-bold overflow-y-hidden">
       {results.reverse().map((yearData, iYear) => {
         return (
-          <section key={iYear} className="App__year">
-            <div className="App__yearText">{yearData.get("year")}</div>
+          <section
+            key={iYear}
+            className="relative snap-start h-screen min-w-full flex flex-col items-center justify-center text-xl text-fox-white bg-winter"
+          >
+            <div
+              className={classNames(
+                "absolute w-screen text-[42px] leading-[42px] text-center z-10 text-white text-shadow font-tilliana",
+                beforeClassNames,
+                afterClassNames
+              )}
+            >
+              {yearData.get("year")}
+            </div>
             <Foxlympics
               isWinter
               teams={yearData.get("teams")}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        winter: "#264653",
+        "fox-white": "#efefef",
+      },
+      textShadow: {
+        DEFAULT: "0px 3px black",
+      },
+    },
+    fontFamily: {
+      tilliana: ["Tillana", "cursive"],
+      gantari: ["Gantari"],
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "text-shadow": (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme("textShadow") }
+      );
+    }),
+  ],
+  important: true,
 };
