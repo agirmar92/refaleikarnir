82956232bf2ac20de304dc3eb411ac64347870d0
aegir <agir.jonsson@matsmart.se>
2023-07-11 23:45:25 +0200

    adding .next dir to gitignore

--- .next/static/chunks/app/layout-d13e7b06576dec03.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{1595:function(n,e,u){Promise.resolve().then(u.t.bind(u,4490,23))},4490:function(){}},function(n){n.O(0,[253,698,744],function(){return n(n.s=1595)}),_N_E=n.O()}]);
