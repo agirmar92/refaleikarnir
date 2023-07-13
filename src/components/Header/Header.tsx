d5b37dcc5dfaf8a7abbec0524ed5bcf8a0977027
aegir <agir.jonsson@matsmart.se>
2023-07-13 14:51:15 +0200

    make header logo a link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,14 @@
+import Link from "next/link";
 import FoxIcon from "@/icons/FoxIcon";
 import MenuIcon from "@/icons/MenuIcon";
 
 const Header = () => {
   return (
     <header className="flex justify-between h-[72px] px-5 items-center z-10 absolute top-0 left-0 w-full">
-      <div className="flex space-x-1">
+      <Link className="flex space-x-1" href="/">
         <FoxIcon />
         <h1 className="small-caps text-xl font-medium">Refaleikarnir</h1>
-      </div>
+      </Link>
       <MenuIcon />
     </header>
   );
