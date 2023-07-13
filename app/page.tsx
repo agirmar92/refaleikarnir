c28037ea06cb5618558de5271cf2962ca55db96d
aegir <agir.jonsson@matsmart.se>
2023-07-13 14:08:09 +0200

    mobile fixed bg-img workaround

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ const RefaleikarnirFrontPage = () => (
   <>
     <div className="w-[100vw] h-[100vw] relative z-0">
       <div className="w-full h-full bg-winter opacity-50 absolute z-10" />
-      <div className="w-full h-full bg-coverPhoto opacity-50 bg-fixed bg-contain" />
+      <div className="w-full h-full bg-coverPhoto opacity-50 bg-contain fixed" />
     </div>
-    <div className="flex flex-col space-y-3 p-3">
+    <div className="flex flex-col space-y-3 p-3 bg-winter relative z-10">
       <div className="w-full h-40 bg-white rounded-[4px]" />
       <div className="w-full h-40 bg-white rounded-[4px]" />
       <div className="w-full h-40 bg-white rounded-[4px]" />
