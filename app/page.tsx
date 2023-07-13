c20ac21a43e9608c6bb5cccd42330e5ff4d56c78
aegir <agir.jonsson@matsmart.se>
2023-07-13 13:46:23 +0200

    init cover photo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+
 const RefaleikarnirFrontPage = () => (
-  <h1 className="h-screen flex flex-col justify-center text-3xl text-center">
-    TODO
-  </h1>
+  <>
+    <div className="w-[100vw] h-[100vw] relative z-0">
+      <div className="w-full h-full bg-winter opacity-50 absolute z-10" />
+      <div className="w-full h-full bg-cover opacity-50 bg-fixed" />
+    </div>
+    <div className="flex flex-col space-y-3 p-3">
+      <div className="w-full h-40 bg-white rounded-[4px]" />
+      <div className="w-full h-40 bg-white rounded-[4px]" />
+      <div className="w-full h-40 bg-white rounded-[4px]" />
+      <div className="w-full h-40 bg-white rounded-[4px]" />
+      <div className="w-full h-40 bg-white rounded-[4px]" />
+    </div>
+  </>
 );
 
 export default RefaleikarnirFrontPage;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import MenuIcon from "@/icons/MenuIcon";
 
 const Header = () => {
   return (
-    <header className="flex justify-between h-[72px] px-5 items-center">
+    <header className="flex justify-between h-[72px] px-5 items-center z-10 absolute top-0 left-0 w-full">
       <div className="flex space-x-1">
         <FoxIcon />
         <h1 className="small-caps text-xl font-medium">Refaleikarnir</h1>
