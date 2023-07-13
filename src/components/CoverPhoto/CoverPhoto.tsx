e3d5081a5851d8caef9f07cc86fa0b5c90e4bf8c
aegir <agir.jonsson@matsmart.se>
2023-07-13 14:46:13 +0200

    fix cover photo overlay positioning

--- src/components/CoverPhoto/CoverPhoto.tsx
+++ src/components/CoverPhoto/CoverPhoto.tsx
@@ -4,7 +4,7 @@ const CoverPhoto = ({ children }: { children: ReactNode }) => {
   return (
     <div className="w-[100vw] h-[100vw] relative z-0">
       <div className="absolute bottom-0 left-0 right-0 z-20 m-3 box-border">{children}</div>
-      <div className="w-full h-full bg-winter opacity-50 absolute z-10" />
+      <div className="w-full h-[100vw] bg-winter opacity-50 fixed z-10" />
       <div className="w-full h-[100vw] bg-coverPhoto opacity-50 bg-contain fixed" />
     </div>
   );
