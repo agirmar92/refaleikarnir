42d6ac139597d26087c4ad5ee8edac196447472e
agirmar92 <agirmar92@gmail.com>
2023-08-30 16:14:42 +0200

    adding a year dropdown selector

--- components/YearSelector/YearSelector.tsx
+++ components/YearSelector/YearSelector.tsx
@@ -6,7 +6,7 @@ import ArrowIcon from "@/icons/ArrowIcon";
 import classNames from "classnames";
 
 const YearSelector = () => {
-  const { currentYear, gamesIndex, stepBack, stepForward } = useYear();
+  const { gamesIndex, stepBack, stepForward, jumpToIndex } = useYear();
   const { season } = results[gamesIndex];
 
   return (
@@ -21,7 +21,27 @@ const YearSelector = () => {
       >
         <ArrowIcon orientation="left" />
       </button>
-      <h2 className="flex-1">{currentYear}</h2>
+      <div className="flex-1 relative">
+        <h2 className="inline-flex items-center relative">
+          {results[gamesIndex].year}
+          <div className="absolute -right-4 h-0 w-0 border-x-[6px] border-x-transparent border-t-[8px] border-t-white"></div>
+        </h2>
+        <select
+          name="pets"
+          className="bg-transparent text-transparent appearance-none text-center absolute top-0 left-0 right-0 bottom-0"
+          onChange={(e) => jumpToIndex(Number.parseInt(e.currentTarget.value))}
+          value={gamesIndex}
+        >
+          {results.map((result, iResult) => (
+            <option key={iResult} value={iResult}>
+              {result.year}
+              {result.season
+                ? ` (${result.season === "summer" ? "sumar" : "vetur"})`
+                : ""}
+            </option>
+          ))}
+        </select>
+      </div>
       {season && (
         <span className="absolute text-sm left-0 bottom-1 w-screen leading-3">
           {season === "summer" ? "Sumar" : "Vetur"}
