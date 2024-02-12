13075511192281b9802f4e19dce2337bdf163a0e
agirmar92 <agirmar92@gmail.com>
2024-02-12 16:21:23 +0100

    refactor: start wining emblems as visible

--- components/YearSelector/YearSelector.tsx
+++ components/YearSelector/YearSelector.tsx
@@ -30,10 +30,10 @@ const YearSelector = ({ gamesIndex }: { gamesIndex: number }) => {
     >
       <div
         className={classNames(
-          'absolute left-0 bottom-[54px] w-full opacity-0 transition-opacity',
+          'absolute left-0 bottom-[54px] w-full opacity-100 transition-opacity',
           {
-            'opacity-100':
-              mainContentWidth && scrollY < mainContentWidth - (64 * 2 + 26),
+            'opacity-0':
+              mainContentWidth && scrollY > mainContentWidth - (64 * 2 + 26),
           }
         )}
       >
