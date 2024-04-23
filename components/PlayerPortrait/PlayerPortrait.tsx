d67c21e814ff7bc6add0617d9d08c5ac6c306fb1
agirmar92 <agirmar92@gmail.com>
2024-04-23 20:38:38 +0200

    feat: player portrait bg color on hover according to team color

--- components/PlayerPortrait/PlayerPortrait.tsx
+++ components/PlayerPortrait/PlayerPortrait.tsx
@@ -1,15 +1,16 @@
-import Link from "next/link";
-import { PlayerDetails, TeamColor } from "@/types";
-import DynamicPlayerPortrait from "./DynamicPlayerPortrait";
+import Link from 'next/link'
+import classNames from 'classnames'
+import { PlayerDetails, TeamColor } from '@/types'
+import DynamicPlayerPortrait from './DynamicPlayerPortrait'
 
 const PlayerPortrait = ({
   player,
   reverseContent = false,
   color,
 }: {
-  player: PlayerDetails;
-  reverseContent?: boolean;
-  color?: TeamColor;
+  player: PlayerDetails
+  reverseContent?: boolean
+  color?: TeamColor
 }) => {
   let content = [
     <p key={`playerName-${player.slug}`}>{player.name}</p>,
@@ -18,19 +19,29 @@ const PlayerPortrait = ({
       player={player}
       color={color}
     />,
-  ];
+  ]
   if (reverseContent) {
-    content = content.reverse();
+    content = content.reverse()
   }
 
   return (
     <Link
-      className="flex items-center space-x-2"
+      className={classNames(
+        'flex items-center space-x-2 rounded-full transition-colors hover:shadow-md',
+        {
+          'pl-3': !reverseContent,
+          'hover:bg-winter hover:text-white': color === undefined,
+          'hover:bg-black hover:text-white': color === 'BLACK',
+          'hover:bg-fox-silver': color === 'SILVER',
+          'hover:bg-fox-red hover:text-white': color === 'RED',
+          'hover:bg-white hover:text-black': color === 'WHITE',
+        }
+      )}
       href={`/player/${player.slug}`}
     >
       {content.map((child) => child)}
     </Link>
-  );
-};
+  )
+}
 
-export default PlayerPortrait;
+export default PlayerPortrait
