5f4352d7326d0617a8ff5efbb12c1ae566697552
agirmar92 <agirmar92@gmail.com>
2024-09-01 12:33:25 +0000

    fix /stats unique key prop error

--- components/PlayersLeaderboard/PlayersLeaderboard.tsx
+++ components/PlayersLeaderboard/PlayersLeaderboard.tsx
@@ -1,53 +1,52 @@
-import { getPlayersWithWinsAndApps } from "@/utils/proccessData";
-import classNames from "classnames";
-import PlayerPortrait from "@/components/PlayerPortrait";
-import { players } from "@/utils/constants";
+import { Fragment } from 'react'
+import { getPlayersWithWinsAndApps } from '@/utils/proccessData'
+import classNames from 'classnames'
+import PlayerPortrait from '@/components/PlayerPortrait'
+import { players } from '@/utils/constants'
 
 const PlayersLeaderboard = () => {
-  const playersWithWinsAndApps = getPlayersWithWinsAndApps();
+  const playersWithWinsAndApps = getPlayersWithWinsAndApps()
 
   return (
     <div className="grid grid-cols-5">
       {playersWithWinsAndApps.map((playerData, iPlayerData) => (
-        <>
+        <Fragment key={`${playerData.slug}-wins-and-apps`}>
           <span
-            className={classNames("col-span-2", {
-              "border-b border-winter pb-3":
+            className={classNames('col-span-2', {
+              'border-b border-winter pb-3':
                 iPlayerData !== playersWithWinsAndApps.length - 1,
-              "pt-3": iPlayerData !== 0,
+              'pt-3': iPlayerData !== 0,
             })}
           >
             <PlayerPortrait player={playerData} reverseContent />
           </span>
           <span
-            key={`${playerData.slug}-wins-and-apps`}
-            className={classNames("col-span-3 flex flex-col justify-center", {
-              "border-b border-winter pb-3":
+            className={classNames('col-span-3 flex flex-col justify-center', {
+              'border-b border-winter pb-3':
                 iPlayerData !== playersWithWinsAndApps.length - 1,
-              "pt-3": iPlayerData !== 0,
+              'pt-3': iPlayerData !== 0,
             })}
           >
-            {playerData.wins} {playerData.wins === 1 ? "sigur" : "sigrar"} í{" "}
-            {playerData.apps} {playerData.apps > 1 ? "leikjum" : "leik"}
+            {playerData.wins} {playerData.wins === 1 ? 'sigur' : 'sigrar'} í{' '}
+            {playerData.apps} {playerData.apps > 1 ? 'leikjum' : 'leik'}
           </span>
-        </>
+        </Fragment>
       ))}
     </div>
-  );
-};
+  )
+}
 
 export const PlayersLeaderboardSkeleton = () => {
-  const allPlayers = Object.keys(players);
+  const allPlayers = Object.keys(players)
 
   return (
     <div className="grid grid-cols-5">
       {allPlayers.map((_, iPlayer) => (
-        <>
+        <Fragment key={`player-skeleton-${iPlayer}`}>
           <span
-            key={`player-${iPlayer}`}
-            className={classNames("col-span-2", {
-              "border-b border-winter pb-3": iPlayer !== allPlayers.length - 1,
-              "pt-3": iPlayer !== 0,
+            className={classNames('col-span-2', {
+              'border-b border-winter pb-3': iPlayer !== allPlayers.length - 1,
+              'pt-3': iPlayer !== 0,
             })}
           >
             <div className="flex items-center space-x-2">
@@ -56,17 +55,17 @@ export const PlayersLeaderboardSkeleton = () => {
             </div>
           </span>
           <span
-            className={classNames("col-span-3  flex flex-col justify-center", {
-              "border-b border-winter pb-3": iPlayer !== allPlayers.length - 1,
-              "pt-3": iPlayer !== 0,
+            className={classNames('col-span-3  flex flex-col justify-center', {
+              'border-b border-winter pb-3': iPlayer !== allPlayers.length - 1,
+              'pt-3': iPlayer !== 0,
             })}
           >
             <div className="bg-gray-400 animate-pulse rounded h-6 w-full" />
           </span>
-        </>
+        </Fragment>
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default PlayersLeaderboard;
+export default PlayersLeaderboard
