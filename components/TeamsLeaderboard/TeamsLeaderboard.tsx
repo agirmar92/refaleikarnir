a765692bbaac7a4c4875d37f70ccaaf6a6c3a871
agirmar92 <agirmar92@gmail.com>
2024-09-18 22:14:59 +0000

    fix: unique key error

--- components/TeamsLeaderboard/TeamsLeaderboard.tsx
+++ components/TeamsLeaderboard/TeamsLeaderboard.tsx
@@ -1,68 +1,67 @@
-import { getTeamColorsWithWins } from "@/utils/proccessData";
-import classNames from "classnames";
-import TeamEmblem from "@/components/TeamEmblem";
-import { actualTeamNames } from "@/utils/constants";
+import { Fragment } from 'react'
+import { getTeamColorsWithWins } from '@/utils/proccessData'
+import classNames from 'classnames'
+import TeamEmblem from '@/components/TeamEmblem'
+import { actualTeamNames } from '@/utils/constants'
 
 const TeamsLeaderboard = () => {
-  const teamColorsWithWins = getTeamColorsWithWins();
+  const teamColorsWithWins = getTeamColorsWithWins()
 
   return (
     <div className="grid grid-cols-3">
       {teamColorsWithWins.map((teamData, iTeamData) => (
-        <>
+        <Fragment key={`${teamData.color}-wins`}>
           <span
-            className={classNames("col-span-2", {
-              "border-b border-winter pb-3":
+            className={classNames('col-span-2', {
+              'border-b border-winter pb-3':
                 iTeamData !== teamColorsWithWins.length - 1,
-              "pt-3": iTeamData !== 0,
+              'pt-3': iTeamData !== 0,
             })}
           >
             <TeamEmblem team={teamData.color} />
           </span>
           <span
-            key={`${teamData.color}-wins`}
-            className={classNames("flex flex-col justify-center", {
-              "border-b border-winter pb-3":
+            className={classNames('flex flex-col justify-center', {
+              'border-b border-winter pb-3':
                 iTeamData !== teamColorsWithWins.length - 1,
-              "pt-3": iTeamData !== 0,
+              'pt-3': iTeamData !== 0,
             })}
           >
             {teamData.wins} sigrar
           </span>
-        </>
+        </Fragment>
       ))}
     </div>
-  );
-};
+  )
+}
 
 export const TeamsLeaderboardSkeleton = () => {
-  const teams = Object.keys(actualTeamNames);
+  const teams = Object.keys(actualTeamNames)
 
   return (
     <div className="grid grid-cols-3">
       {teams.map((_, iTeam) => (
-        <>
+        <Fragment key={`team-${iTeam}`}>
           <span
-            key={`team-${iTeam}`}
-            className={classNames("col-span-2", {
-              "border-b border-winter pb-3": iTeam !== teams.length - 1,
-              "pt-3": iTeam !== 0,
+            className={classNames('col-span-2', {
+              'border-b border-winter pb-3': iTeam !== teams.length - 1,
+              'pt-3': iTeam !== 0,
             })}
           >
             <div className="h-24 w-16 m-auto bg-gray-400 animate-pulse rounded" />
           </span>
           <span
-            className={classNames("flex flex-col justify-center", {
-              "border-b border-winter pb-3": iTeam !== teams.length - 1,
-              "pt-3": iTeam !== 0,
+            className={classNames('flex flex-col justify-center', {
+              'border-b border-winter pb-3': iTeam !== teams.length - 1,
+              'pt-3': iTeam !== 0,
             })}
           >
             <div className="bg-gray-400 animate-pulse rounded h-6 w-2/3" />
           </span>
-        </>
+        </Fragment>
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default TeamsLeaderboard;
+export default TeamsLeaderboard
