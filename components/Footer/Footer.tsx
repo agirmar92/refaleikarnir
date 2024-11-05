062ee59d9323fa4c8bd83c376393de0979c0d438
Ægir Már Jónsson <agirmar92@gmail.com>
2024-11-05 21:07:43 +0000

    feat: add foxel logo (#316)

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,19 +1,19 @@
-import GithubIcon from "@/icons/GithubIcon";
+import { FoxelLogo } from '@/icons/FoxelLogo'
+import GithubIcon from '@/icons/GithubIcon'
 
 const Footer = () => (
-  <footer className="relative z-10">
+  <footer className="flex z-10 h-20 justify-between px-5 py-2">
+    <FoxelLogo />
     <a
       target="_blank"
       href="https://github.com/agirmar92/refaleikarnir"
       rel="noopener noreferrer"
-      className="flex justify-center py-3 text-stone-300"
+      className="flex items-center space-x-2 text-stone-300"
     >
-      agirmar92/refaleikarnir
-      <span className="ml-2">
-        <GithubIcon />
-      </span>
+      <span>agirmar92/refaleikarnir</span>
+      <GithubIcon />
     </a>
   </footer>
-);
+)
 
-export default Footer;
+export default Footer
