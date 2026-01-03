82cb0eb5ae65de4bf68be7f63a7c6f28125eb005
agirmar92 <agirmar92@gmail.com>
2026-01-03 12:54:40 +0000

    update foxel logo in footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,16 +1,22 @@
-import { FoxelLogo } from '@/icons/FoxelLogo'
+import Image from 'next/image'
 import GithubIcon from '@/icons/GithubIcon'
 
 const Footer = () => (
   <footer className="flex z-10 h-20 justify-between px-5 py-2">
-    <FoxelLogo />
+    <Image
+      src="/foxel_logo.png"
+      alt="Foxel logo"
+      width={878}
+      height={357}
+      className="h-full w-auto"
+    />
     <a
       target="_blank"
       href="https://github.com/agirmar92/refaleikarnir"
       rel="noopener noreferrer"
       className="flex items-center space-x-2 text-stone-300"
     >
-      <span>agirmar92/refaleikarnir</span>
+      <span className="hidden sm:inline-block">agirmar92/refaleikarnir</span>
       <GithubIcon />
     </a>
   </footer>
